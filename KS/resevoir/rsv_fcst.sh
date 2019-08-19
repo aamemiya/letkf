9e95201f73350a68d57b2ad4bdf0383d79e92647
Arata Amemiya <arata.amemiya@riken.jp>
2019-08-20 06:14:07 +0900

    add resevoir computing

---
+++ KS/resevoir/rsv_fcst.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#set -e
+F90=ifort
+VAR=''
+CDIR=`pwd`
+cd ..
+KSDIR=`pwd`
+cd ..
+ENKFDIR=`pwd`
+COMDIR=$ENKFDIR/common
+NATDIR=$KSDIR/DATA
+OUTDIR=$KSDIR/DATA/fcst
+WKDIR=$KSDIR/tmp
+TESTDIR=$KSDIR/test
+rm -rf $WKDIR
+mkdir -p $WKDIR
+cd $WKDIR
+cp $CDIR/rsv_fcst.f90 .
+cp $COMDIR/SFMT.f90 .
+cp $COMDIR/netlib.f .
+cp $COMDIR/common.f90 .
+cp $COMDIR/common_mtx.f90 .
+cp $COMDIR/common_reg.f90 .
+cp $COMDIR/common_rsv_rnet.f90 .
+cp $NATDIR/nature_train.dat ./fort.10
+cp $NATDIR/nature_test.dat ./fort.30
+$F90 -o rsv_fcst SFMT.f90 netlib.f common.f90 common_mtx.f90 common_reg.f90 common_rsv_rnet.f90 rsv_fcst.f90 -mkl
+rm *.mod
+time ./rsv_fcst
+mkdir -p $OUTDIR
+mv fort.90 $OUTDIR/fcst.dat
