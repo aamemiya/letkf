abee2a2ef6ed857a0ab863e52024eae1d29dc006
Shlok Mohta <shlokmohta@cspp.cc.u-tokyo.ac.jp>
2019-09-05 14:24:04 +0900

    Made some changes in network arch.

--- Python_files/network_arch.py
+++ Python_files/network_arch.py
@@ -3,7 +3,21 @@ import tensorflow as tf
 
 #Model defination
 def rnn_model(parameter_list):
-    net_input = tf.keras.Input(shape=(parameter_list['time_splits'], parameter_list['locality']), name='INPUT')
-    x = tf.keras.layers.LSTM(units=parameter_list['LSTM_output'], return_sequences=True)(net_input)
-    output = tf.keras.layers.Dense(units=parameter_list['net_output'], activation=tf.keras.activations.relu)(x)
+    net_input = tf.keras.Input(shape=(parameter_list['time_splits'],
+                            parameter_list['locality']),
+                            name='INPUT')
+
+    x = tf.keras.layers.LSTM(units=parameter_list['LSTM_output'],
+                            activation = parameter_list['activation'],
+                            recurrent_activation = parameter_list['rec_activation']
+                            kernel_regularizer = tf.keras.regularizers.l2(parameter_list['l2_regu']),
+                            activity_regularizer = tf.keras.regularizers.l1(parameter_list['l1_regu']),
+                            dropout = parameter_list['lstm_dropout'],
+                            recurrent_dropout = parameter_list['rec_lstm_dropout'],
+                            unroll = parameter_list['unroll_lstm'],
+                            return_sequences=True)(net_input)
+
+    output = tf.keras.layers.Dense(units=parameter_list['net_output'],
+                            activation=tf.keras.activations.relu,
+                            )(x)
     return tf.keras.Model(net_input, output, name='RNN')
--- Python_files/params.py
+++ Python_files/params.py
@@ -1,14 +1,21 @@
 #Parameter List
 parameter_list = {}
 
+#Dataset related settings
 parameter_list['netCDf_loc'] = "../lorenz96_multi/DATA_sample/X40F18/all_10/nocorr_I20/assim.nc"
 parameter_list['xlocal'] = 3
 parameter_list['locality'] = 19
 parameter_list['time_splits'] = 30
+parameter_list['tfrecord_analysis'] = './analysis.tfrecord'
+parameter_list['tfrecord_forecast'] = './forecast'
 parameter_list['batch_size'] = 250
 parameter_list['val_size'] = 2
+
+#Network related settings
 parameter_list['LSTM_output'] = 5
 parameter_list['net_output'] = 1
+
+#Training related settings
 parameter_list['learning_rate'] = 1e-3
 parameter_list['log_dir'] = './log'
 parameter_list['checkpoint_dir'] = './checkpoint'
@@ -18,6 +25,3 @@ parameter_list['log_freq'] = 5
 parameter_list['early_stop_patience'] = 5
 parameter_list['num_epochs_checkpoint'] = 2
 parameter_list['summery_freq'] = 1
-
-parameter_list['tfrecord_analysis'] = './analysis.tfrecord'
-parameter_list['tfrecord_forecast'] = './forecast'
